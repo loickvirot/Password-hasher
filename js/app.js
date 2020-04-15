2c238eb4d29885342028b840ccd8ac33917b9045
LoickVirot <loick.virot@gmail.com>
2020-04-15 21:31:07 +0200

    automatic date in footer

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
-function updateTitle(value){
-  $('#lenght').attr("title", value)
-}
-
 $(document).ready(function(){
   $('#lenght').val(16)
-  updateTitle($('#lenght').val())
-  var input = $("hashContent");
+  var input = $("#hashContent");
   var button = $( "button" );
+
+  footerDate();
 
   button.click(function(){
     var hash = $.base64.btoa(CryptoJS.SHA256($("#prefix").val() + $("#service").val().toLowerCase() + $("#suffix").val()))
     if ($('#lenght').val() == 0) {
-      $("#hashContent").val(hash);
-    }else {
+      input.val(hash);
+    } else {
       $("#hashContent").val(hash.slice(0, $('#lenght').val()/2) + hash.slice(-$('#lenght').val()/2 ,hash.length));
     }
   });
 });
+
+function footerDate() {
+  var date = new Date()
+  $('#currentYear').html(' - '  + date.getFullYear())
+}
