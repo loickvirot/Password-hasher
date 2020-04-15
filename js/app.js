78d0114972dea891fbe4624a45228f08b6bb66eb
LoickVirot <loick.virot@gmail.com>
2020-04-15 22:51:55 +0200

    Redesign and improve user experience

--- js/app.js
+++ js/app.js
@@ -1,17 +1,46 @@
 $(document).ready(function(){
   $('#lenght').val(16)
-  var input = $("#hashContent");
-  var button = $( "button" );
+  var hashContent = $("#hashContent");
+  var generateBtn = $( "#generateBtn" );
+  var copyBtn = $( "#copyBtn" );
+  var form = $('.form');
 
   footerDate();
 
-  button.click(function(){
+  form.submit(function() {
+    return false;
+  })
+
+  generateBtn.click(function(){
     var hash = $.base64.btoa(CryptoJS.SHA256($("#prefix").val() + $("#service").val().toLowerCase() + $("#suffix").val()))
     if ($('#lenght').val() == 0) {
-      input.val(hash);
+      hashContent.val(hash);
     } else {
       $("#hashContent").val(hash.slice(0, $('#lenght').val()/2) + hash.slice(-$('#lenght').val()/2 ,hash.length));
     }
+  });
+
+  copyBtn.click(function(e) {;
+    var el = hashContent[0];
+    var oldContentEditable = el.contentEditable,
+        oldReadOnly = el.readOnly,
+        range = document.createRange();
+
+    el.contentEditable = true;
+    el.readOnly = false;
+    range.selectNodeContents(el);
+
+    var s = window.getSelection();
+    s.removeAllRanges();
+    s.addRange(range);
+
+    el.select();
+    el.setSelectionRange(0, 999999); // A big number, to cover anything that could be inside the element.
+
+    el.contentEditable = oldContentEditable;
+    el.readOnly = oldReadOnly;
+
+    document.execCommand('copy');
   });
 });
 
