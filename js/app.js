bdb7e7c5362639ac37c0d8fa25d4ace676959af6
LoickVirot <loick.virot@gmail.com>
2020-04-20 11:12:42 +0200

    Add PWA support

--- js/app.js
+++ js/app.js
@@ -56,6 +56,20 @@ $(document).ready(function(){
 
     $('.toast').toast('show');
   });
+
+  // REGISTER SERVICE WORKER
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js',
+    {
+      scope: '/'
+    })
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    })
+    .catch(function(error) {
+      console.log('Service worker registration failed, error:', error);
+    });
+  }
 });
 
 function footerDate() {
